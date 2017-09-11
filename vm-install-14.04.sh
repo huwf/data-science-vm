4642705a446c8bb978890baeb9cb0c5901eda0ae
Huw Fryer <huwfryer@gmail.com>
2017-09-11 13:13:16 +0100

    First commit

---
+++ vm-install-14.04.sh
@@ -0,0 +1,140 @@
+#!/usr/bin/env bash
+
+# VM Install for Ubuntu 14.04
+# ===========================
+
+# Install Git and Docker:
+sudo apt-get install git
+
+# INSTALL DOCKER-CE
+sudo apt-get install  apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+# Assuming x86 (64 bit)
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+# Might already be installed on some systems.
+sudo apt-get install -y docker-ce
+
+# Gives user permissions to run docker
+# Needs to log out and log in again before this will work
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# INSTALL docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Install Anaconda
+cd /tmp
+curl -O https://repo.continuum.io/archive/Anaconda3-4.4.0-Linux-x86_64.sh
+conda install -y notebook='5.0.0'
+
+# Add Python 2 environment
+conda create -n py27 python=2.7 anaconda
+source activate py27
+conda install -y nb_conda='2.2.0'
+
+# Add R
+conda config --system --add channels r && \
+    conda install --quiet --yes \
+    'rpy2=2.8*' \
+    'r-base=3.3.2' \
+    'r-irkernel=0.7*' \
+    'r-plyr=1.8*' \
+    'r-devtools=1.12*' \
+    'r-tidyverse=1.0*' \
+    'r-shiny=0.14*' \
+    'r-rmarkdown=1.2*' \
+    'r-forecast=7.3*' \
+    'r-rsqlite=1.1*' \
+    'r-reshape2=1.4*' \
+    'r-nycflights13=0.2*' \
+    'r-caret=6.0*' \
+    'r-rcurl=1.95*' \
+    'r-crayon=1.3*' \
+    'r-randomforest=4.6*' && \
+conda clean -tipsy
+
+# Install Docker
+sudo apt-get update
+
+# INSTALL DOCKER-CE
+sudo apt-get install  apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+# Assuming x86 (64 bit)
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+# Might already be installed on some systems.
+sudo apt-get install -y docker-ce
+
+# Gives user permissions to run docker
+# Needs to log out and log in again before this will work
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# INSTALL docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# INSTALL Java/Hadoop/Spark/PySpark
+sudo apt-get install openjdk-7-jre-headless ca-certificates-java
+
+# Install Java 8 (Oracle) from webupd8
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo update-alternatives --config java
+java -version
+
+cd /usr/local 
+export APACHE_SPARK_VERSION=2.2.0
+export HADOOP_VERSION=2.7
+wget -q http://d3kbcqa49mib13.cloudfront.net/spark-${APACHE_SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz
+tar xzf spark-${APACHE_SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz -C /usr/local
+sudo ln -s spark-${APACHE_SPARK_VERSION}-bin-hadoop${HADOOP_VERSION} spark
+
+wget http://www-eu.apache.org/dist/hadoop/common/hadoop-2.7.4/hadoop-2.7.4.tar.gz
+sudo tar zxf hadoop-2.7.4.tar.gz
+sudo rm hadoop-2.7.4.tar.gz
+
+sudo echo "
+export HADOOP_HOME=/usr/local/hadoop-2.7.4
+export SPARK_HOME=/usr/local/spark
+export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$SPARK_HOME/bin:$SPARK_HOME/sbin
+" >> ~/.bashrc
+
+source ~/.bashrc
+
+# INSTALL mongo 3.2
+# https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+# INSTALL Node JS
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install npm --global
+
+echo "
+export NPM_PACKAGES=\"${HOME}/.npm-packages\"
+
+export PATH=\"$NPM_PACKAGES/bin:$PATH\"
+" >> ${HOME}/.bashrc
+
+npm config set prefix '~/.npm-packages'
+npm install -g http-server bower
+bower install jQuery d3 underscore
