a9a5506b5c712f64cc7ac89903bb0aa803c1346e
Huw Fryer <huwfryer@gmail.com>
2017-09-14 13:26:04 +0100

    Changes with .bashrc

--- vm-install-14.04.sh
+++ vm-install-14.04.sh
@@ -27,8 +27,9 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 
 # INSTALL docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+curl -O https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m`
+sudo mv ./docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Install Anaconda
 # cd /tmp
@@ -96,14 +97,6 @@ wget http://www-eu.apache.org/dist/hadoop/common/hadoop-2.7.4/hadoop-2.7.4.tar.g
 sudo tar zxf hadoop-2.7.4.tar.gz
 sudo rm hadoop-2.7.4.tar.gz
 
-sudo echo "
-export HADOOP_HOME=/usr/local/hadoop-2.7.4
-export SPARK_HOME=/usr/local/spark
-export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$SPARK_HOME/bin:$SPARK_HOME/sbin
-" >> ~/.bashrc
-
-source ~/.bashrc
-
 # INSTALL mongo 3.2
 # https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
@@ -115,16 +108,21 @@ sudo apt-get install -y mongodb-org
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install npm --global
-mkdir .npm-packages
-echo "
-export NPM_PACKAGES=\"${HOME}/.npm-packages\"
+mkdir ~/.npm-packages
 
-export PATH=\"$NPM_PACKAGES/bin:$PATH\"
+# Set all environment variables before we run npm and bower
+ssudo echo "
+export HADOOP_HOME=\"/usr/local/hadoop-2.7.4\"
+export SPARK_HOME=\"/usr/local/spark\"
+export PATH=\"\$PATH:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin:\$SPARK_HOME/bin:\$SPARK_HOME/sbin\"
+export NPM_PACKAGES=\"\$HOME/.npm-packages\"
+export PATH=\"\$NPM_PACKAGES/bin:\$PATH\"
 " >> ~/.bashrc
+
 source ~/.bashrc
 
-sudo chown user:user ~/.npm-packages
+sudo chown $USER:$USER ~/.npm-packages
 
 npm config set prefix '~/.npm-packages'
-npm install -g http-server bower
-bower install jQuery d3 underscore
+npm install -g http-server 
+npm install -g bower
