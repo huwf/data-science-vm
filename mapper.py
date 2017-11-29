ef67f962ecc560c8db504f50854fead043a4d1fe
Huw <huwfryer@gmail.com>
2017-11-29 16:28:21 +0000

    Adding stuff for tutorial 2

---
+++ mapper.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# MAPPER
+
+import csv
+import sys
+
+input_text = open('Youtube04-Eminem.csv', 'r')
+# When we move to the actual MapReduce job, we will need to read from STDIN
+input_text = sys.stdin
+
+reader = csv.reader(input_text)
+# Skip the column header
+next(reader)
+for row in reader:
+    tokens = row[3].replace('to', 'to and to').split(' ')
+    for t in tokens:
+        # print tab delimted here,
+        # will be input for the reducer
+        print('%s\t%d' % (t, 1))    
+    # Only do it for the first record for now
+    break
+
+# input_text.close()
---
+++ reducer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# REDUCER
+
+import sys
+# Keep simple example in for now, switch to stdin later
+
+input_text = sys.stdin
+words = {}
+
+for line in input_text:
+    #print('line: ', line)
+    if line.strip() == '':
+        continue
+    word, count = line.split('\t', 1)
+    #print('word: %s count: %s' % (word, count))
+    
+    # Convert count to an integer
+    try:
+        count = int(count)
+    except ValueError:
+        # We can safely ignore, so keep calm and carry on
+        continue
+        
+        
+    if word in words:
+        words[word] += 1
+    else:
+        words[word] = 1
+        
+for w in words:
+    print('%s\t%s' % (w, words[w]))
