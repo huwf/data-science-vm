4fa47c4f0d93bde09c342e75abd8603b587bfb0b
Huw Fryer <huwfryer@gmail.com>
2017-11-29 15:54:13 +0000

    Corrected typos, and added mkdir and chmod for logs dir

--- hadoop-config.sh
+++ hadoop-config.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# We seem to need this, otherwise it breaks from permission error
+sudo mkdir -p /usr/local/hadoop-2.7.4/logs
+sudo chown -R user:user /usr/local/hadoop-2.7.4/logs
+
 path=/usr/local/hadoop-2.7.4/etc/hadoop
 echo $path
 
@@ -9,3 +13,6 @@ for file in core-site.xml hadoop-env.sh mapred-site.xml yarn-site.xml hdfs-site.
     wget -O $output https://raw.githubusercontent.com/huwf/data-science-vm/master/etc/hadoop/$file
     cat $output
 done
+
+mkdir -p /usr/local/hadoop-2.7.4/logs
+chown -R user:user /usr/local/hadoop-2.7.4/logs
